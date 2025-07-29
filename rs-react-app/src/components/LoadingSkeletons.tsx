269e99e57e81f5248c2b24c5aab7f633e4266dcb
Anastasiia Pashkevych <anastasiia.pashk@gmail.com>
2025-07-29 08:01:22 -0400

    refactor: convert LoadingSkeletons component to functional component

--- rs-react-app/src/components/LoadingSkeletons.tsx
+++ rs-react-app/src/components/LoadingSkeletons.tsx
@@ -1,18 +1,18 @@
-import { Component } from 'react';
+import type { FC } from 'react';
 
-export default class LoadingSkeletons extends Component {
-  render() {
-    return (
-      <div className="flex flex-wrap gap-4">
-        {Array.from({ length: 6 }).map((_, i) => (
-          <div
-            key={i}
-            role="presentation"
-            aria-label="Loading character placeholder"
-            className="w-[150px] h-[250px] bg-gray-300 rounded-lg animate-pulse duration-1500"
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const LoadingSkeletons: FC = () => {
+  return (
+    <div className="flex flex-wrap gap-4">
+      {Array.from({ length: 6 }).map((_, i) => (
+        <div
+          key={i}
+          role="presentation"
+          aria-label="Loading character placeholder"
+          className="w-[150px] h-[250px] bg-gray-300 rounded-lg animate-pulse duration-[1500ms]"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default LoadingSkeletons;
