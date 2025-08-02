9b36e40b0f44b2512f9e0efcfdc475ea4146b5d4
Anastasiia Pashkevych <anastasiia.pashk@gmail.com>
2025-08-01 22:03:55 -0400

    refactor: convert ErrorButton component to functional component

--- rs-react-app/src/components/ErrorButton.tsx
+++ rs-react-app/src/components/ErrorButton.tsx
@@ -1,31 +1,20 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-interface State {
-  throwError: boolean;
-}
+const ErrorButton = () => {
+  const [throwError, setThrowError] = useState(false);
 
-export default class ErrorButton extends Component<
-  Record<string, never>,
-  State
-> {
-  state: State = { throwError: false };
-
-  handleClick = (): void => {
-    this.setState({ throwError: true });
-  };
-
-  render() {
-    if (this.state.throwError) {
-      throw new Error('Test error from ErrorButton');
-    }
-
-    return (
-      <button
-        onClick={this.handleClick}
-        className="mt-5 px-4 py-2 bg-red-500 text-black font-semibold rounded hover:bg-red-600 transition duration-200"
-      >
-        Throw Error
-      </button>
-    );
+  if (throwError) {
+    throw new Error('Test error from ErrorButton');
   }
-}
+
+  return (
+    <button
+      onClick={() => setThrowError(true)}
+      className="mt-5 px-4 py-2 bg-red-500 text-black font-semibold rounded hover:bg-red-600 transition duration-200"
+    >
+      Throw Error
+    </button>
+  );
+};
+
+export default ErrorButton;
