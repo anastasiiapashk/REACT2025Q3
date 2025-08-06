a0db9cbb79259ccf516e50c2e408aa5778d5b6da
Anastasiia Pashkevych <anastasiia.pashk@gmail.com>
2025-08-06 10:47:13 -0400

    fix: fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary.tsx';
 import HomePage from './pages/HomePage.tsx';
-import Details from './components/Details.tsx';
 import AboutPage from './pages/AboutPage.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 
@@ -10,10 +9,9 @@ const App = () => {
     <BrowserRouter>
       <ErrorBoundary>
         <Routes>
-          <Route path="/" element={<HomePage />}>
-            <Route path=":page" element={<Details />} />
-            <Route path=":page/:id" element={<Details />} />
-          </Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path=":page" element={<HomePage />} />
+          <Route path=":page/:id" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
